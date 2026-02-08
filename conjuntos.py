5a2c95f01581ce15b82ad379fb4d94bd399f28ba
Angelica <angelicasilvaalves03@gmail.com>
2026-02-08 02:15:50 -0300

    finalizando código

--- conjuntos.py
+++ conjuntos.py
@@ -24,11 +24,13 @@ def gerar_conjunto_B():
 
     return conjunto_B
 
+while True:
 
     conjunto_A = []
 
     print("Entre com os valores do conjunto A:")
 
+    while len(conjunto_A) < MAX:
 
         entrada = input(f"Elemento {len(conjunto_A)+1}: ").strip()
 
@@ -55,48 +57,50 @@ def gerar_conjunto_B():
             if parar == "s":
                 break
 
- 	# Geração automática do conjunto B
-    	conjunto_B = gerar_conjunto_B()
+    # Geração automática do conjunto B
+    conjunto_B = gerar_conjunto_B()
 
-    	set_A = set(conjunto_A)
-    	set_B = set(conjunto_B)
+    set_A = set(conjunto_A)
+    set_B = set(conjunto_B)
 
-    	# Operações entre conjuntos
-    	uniao = set_A | set_B
-    	intersecao = set_A & set_B
-    	diferenca = set_A - set_B
-    	diferenca_BA = set_B - set_A
-    	delta = set_A ^ set_B  # diferença simétrica
+    # Operações entre conjuntos
+    uniao = set_A | set_B
+    intersecao = set_A & set_B
+    diferenca = set_A - set_B
+    diferenca_BA = set_B - set_A
+    delta = set_A ^ set_B  # diferença simétrica
 
-    	# Módulos
-    	modulo_A = len(set_A)
-    	modulo_B = len(set_B)
-    	uniao_modulos = {modulo_A, modulo_B}
+    # Módulos
+    modulo_A = len(set_A)
+    modulo_B = len(set_B)
+    uniao_modulos = {modulo_A, modulo_B}
 
-    	# Saída
-    	print("\n--- RESULTADO ---")
-    	print("Conjunto A:", set_A)
-    	print("Conjunto B:", set_B)
+    # Saída
+    print("\n--- RESULTADO ---")
+    print("Conjunto A:", set_A)
+    print("Conjunto B:", set_B)
 
-    	print("\n(A ∪ B):", uniao)
+    print("\n(A ∪ B):", uniao)
 
-    	if intersecao:
-        	print("(A ∩ B):", intersecao)
-    	else:
-        	print("(A ∩ B): ∅")
+    if intersecao:
+        print("(A ∩ B):", intersecao)
+    else:
+        print("(A ∩ B): ∅")
 
-    	print("(A - B):", diferenca)
-    	print("(B - A):", diferenca_BA)
-    	print("(A Δ B):", delta)
+    print("(A - B):", diferenca)
+    print("(B - A):", diferenca_BA)
+    print("(A Δ B):", delta)
 
-    	print("|A|:", modulo_A)
-    	print("|B|:", modulo_B)
-    	print("|A ∪ B|:", uniao_modulos)
+    print("|A|:", modulo_A)
+    print("|B|:", modulo_B)
+    print("|A ∪ B|:", uniao_modulos)
 
-    	comando = input(
-        	"\nDigite 'again' ou 'novamente' para limpar e continuar.\n"
-       	 "Digite 'sair' ou 'exit' para encerrar.\n"
-    	).lower()
+    comando = input(
+        "\nDigite 'again' ou 'novamente' para limpar e continuar.\n"
+        "Digite 'sair' ou 'exit' para encerrar.\n"
+    ).lower()
 
-    	if comando in ["sair", "exit"]:
-      	 break
+    if comando in ["sair", "exit"]:
+        break
+
+    limpar_tela()
